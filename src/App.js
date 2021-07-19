60445fb836561e8c19fedd244dd1c087e44d28ab
raimondas-27 <raimondasvilnius@yahoo.com>
2021-07-19 11:19:46 +0300

    mobile-plan card done, plan1.json keys added to mobile-plan

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Commitment from "./Components/Commitment";
 import HaveServices from "./Components/HaveServices";
 import MobilePlan from "./Components/Mobile-plan";
+import 'font-awesome/css/font-awesome.css';
 
 
 
@@ -12,10 +13,16 @@ class App extends Component {
    render() {
       return (
           <div className="App">
-             <Commitment/>
-             <HaveServices/>
-             <MobilePlan/>
-          </div>
+        <h1>Mobiliojo ryšio planai</h1>
+        <p>Visos Lietuvoje turimo plano naudos galioja Baltijos ir Skandinavijos šalyse.</p>
+        <div className="controls">
+          <Commitment />
+          <HaveServices />
+        </div>
+        <main className="plan-cards">
+          <MobilePlan />
+        </main>
+      </div>
       );
    }
 }
--- src/Components/Mobile-plan.js
+++ src/Components/Mobile-plan.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
+import * as data from "../Data/plan1.json";
 
 class MobilePlan extends Component {
    state = {}
 
    render() {
+
       return (
           <div className={"cart-container"}>
-             <div className={"gigabytes-section"}>
-                <h3> 2 GB </h3>
-                <p> Is ju 1 GB ES/EEE </p>
-                <p> Neribotos MIN ir SMS </p>
+             <div className={"payment-card"}>
+                <div className={"gigabytes-section"}>
+                   <div className={"gigabytes-section__name"}>
+                      <p> {data.headerTitle} </p>
+                   </div>
+                   <div className={"gigabytes-section__data"}>
+                      <span className={"gigabytes-section_data-old"}> {data.dataAlowed} GB </span>
+                      <span className={"gigabytes-section_data-new"}> 2 GB </span>
+                   </div>
+                   <div className={"gigabytes-section__description"}>
+                      <p className={"gigabytes-section__description-countries"}> Is ju {data.dataEU} GB ES/EEE </p>
+                      <p className={"gigabytes-section__description-callsMessages"}> {data.minSms} </p>
+                   </div>
+                </div>
+                <div className={"advantages-section"}>
+                   <div className={"advantages-section__services"}>
+                      {data.features.map((oneFeature) => (
+                          <i className="fa fa-check">
+                             <span>{oneFeature.title}</span>
+                          </i>
+                         ))}
+                   </div>
+                </div>
+                <div className={"price-section"}>
+                   <div className={"price-section__aligned"}>
+                      <div className={"price-section__description"}>
+                         <p className={"price-section__price"}> {data.price.commitment} <span>&euro;/men</span></p>
+                         <p className={"price-section__length"}> {data.contractLengh} {data.period} sutartis </p>
+                      </div>
+                      <button className={"price-section__button"}> {data.ctaButton}</button>
+                   </div>
+                </div>
              </div>
           </div>
       );
